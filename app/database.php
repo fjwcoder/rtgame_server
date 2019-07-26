e701a7a0aa76aae34c98955387cd7c3ecf5db125
fjwcoder <fjwcoder@gmail.com>
2019-07-26 14:40:07 +0800

    ……

--- app/database.php
+++ app/database.php
@@ -4,14 +4,14 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    // 'hostname'        => '127.0.0.1',  
-    'hostname'        => '140.249.17.72', 'password'        => 'ruitong2019',
+    'hostname'        => '127.0.0.1',  
+    // 'hostname'        => '47.105.205.37', 'password'        => 'ruitong2019',
     // 数据库名
     'database'        => 'qijian',
     // 用户名
     'username'        => 'root',
     // 密码
-    // 'password'        => '19920104',
+    'password'        => '19920104',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
